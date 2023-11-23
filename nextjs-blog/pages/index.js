d9e4d949904b5a7f95ae98c32709e37d6e177d30
mic-ilo <michelaquino1003@gmail.com>
2023-11-23 14:35:57 +0800

    pre-rendering and data fetching

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,19 +1,50 @@
-import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
 
-export default function Home() {
+export async function getStaticProps() {
+  //If API endpoint
+  //cosnt res = await fetch('')
+  //return res.json()
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello I'm Michel. I'm a software engineer</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
+          Hello I'm <span className={utilStyles.bold}>Michel</span>. I'm a
+          software engineer
+        </p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
